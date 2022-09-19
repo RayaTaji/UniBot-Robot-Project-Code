db5085bf11b3394c09d7240ea1984d6cf8c94b45
Raya <110840012+RayaTaji@users.noreply.github.com>
2022-09-19 21:26:56 +0300

    Code Tidying

--- robot.py
+++ robot.py
@@ -4,8 +4,7 @@ import sys
 import tkinter as tk
 from sensor import distance
 import random
-import AAU_Uni_Bot
-
+import main
 
 
 def init():
@@ -16,113 +15,99 @@ def init():
     gpio.setup(15, gpio.OUT)
 
 
-
-#These functions enable and disable pins based on the direction the robot is going
+# These functions enable and disable pins based on the direction the robot is going
 def forward(tf):
-    gpio.output (7, True)
-    gpio.output (11, False)
-    gpio.output (13, False)
-    gpio.output (15, True)
+    gpio.output(7, True)
+    gpio.output(11, False)
+    gpio.output(13, False)
+    gpio.output(15, True)
     time.sleep(tf)
     gpio.cleanup()
-    
+
+
 def stop(tf):
-    gpio.output (7, False)
-    gpio.output (11, False)
-    gpio.output (13, False)
-    gpio.output (15, False)
+    gpio.output(7, False)
+    gpio.output(11, False)
+    gpio.output(13, False)
+    gpio.output(15, False)
     time.sleep(tf)
     gpio.cleanup()
 
 
-
-   
 def reverse(tf):
-    gpio.output (7, False)
-    gpio.output (11, True)
-    gpio.output (13, True)
-    gpio.output (15, False)
+    gpio.output(7, False)
+    gpio.output(11, True)
+    gpio.output(13, True)
+    gpio.output(15, False)
     time.sleep(tf)
     gpio.cleanup()
-
 
 
 def turn_left(tf):
-    gpio.output (7, True)
-    gpio.output (11, True)
-    gpio.output (13, True)
-    gpio.output (15, False)
+    gpio.output(7, True)
+    gpio.output(11, True)
+    gpio.output(13, True)
+    gpio.output(15, False)
     time.sleep(tf)
     gpio.cleanup()
-
 
 
 def turn_right(tf):
-    gpio.output (7, False)
-    gpio.output (11, True)
-    gpio.output (13, False)
-    gpio.output (15, False)
+    gpio.output(7, False)
+    gpio.output(11, True)
+    gpio.output(13, False)
+    gpio.output(15, False)
     time.sleep(tf)
     gpio.cleanup()
-
-
 
 
 def pivot_left(tf):
-    gpio.output (7, False)
-    gpio.output (11, True)
-    gpio.output (13, False)
-    gpio.output (15, True)
+    gpio.output(7, False)
+    gpio.output(11, True)
+    gpio.output(13, False)
+    gpio.output(15, True)
     time.sleep(tf)
     gpio.cleanup()
-
 
 
 def pivot_right(tf):
-    gpio.output (7, False)
-    gpio.output (11, True)
-    gpio.output (13, False)
-    gpio.output (15, True)
+    gpio.output(7, False)
+    gpio.output(11, True)
+    gpio.output(13, False)
+    gpio.output(15, True)
     time.sleep(tf)
     gpio.cleanup()
 
 
-
-
-#calculates the distance and prints it on the screen
+# calculates the distance and prints it on the screen
 def check_front():
-      init()
-      dist = distance()
-
-
-
-     if dist < 15:
-          print('Too close,',dist)
-          init()
-          reverse(2)
-          dist = distance()
-          if dist < 15:
-              print('Too close,',dist)
-              init()
-              pivot_left(3)
-              init()
-              reverse(2)
-              dist = distance()
-              if dist < 15:
-                  print('Too close, giving up',dist)
-                  sys.exit()
+    init()
+    dist = distance()
+    if dist < 15:
+            print('Too close,',dist)
+            init()
+            reverse(2)
+            dist = distance()
+            if dist < 15:
+                print('Too close,',dist)
+                init()    
+                pivot_left(3)
+                init()
+                reverse(2)
+                dist = distance()
+                if dist < 15:
+                    print('Too close, giving up',dist)
+                    sys.exit()
 
 
 
 
-#moves robot based on the check_front function, then takes the right action like turn left/reverse, etc
+# moves robot based on the check_front function, then takes the right action like turn left/reverse, etc
 def autonomy():
     tf = 0.030
     x = random.randrange(0,4)
 
-
-
-   if x == 0:
+    if x == 0:
         for y in range(30):
             check_front()
             init()
